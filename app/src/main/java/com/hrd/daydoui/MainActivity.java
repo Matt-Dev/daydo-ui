b9f9b64887bd8cb838e5cab7a0066d546ce7bcf3
Rorpheeyah <mathrorpheeyah@gmail.com>
2020-01-01 20:34:16 +0700

    finish bottom navigation bar,language

--- app/src/main/java/com/hrd/daydoui/MainActivity.java
+++ app/src/main/java/com/hrd/daydoui/MainActivity.java
@@ -1,10 +1,12 @@
 package com.hrd.daydoui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitleTextColor(getColor(R.color.colorBlueDark));
+        toolbar.setLogo(R.drawable.ic_menu);
+        setSupportActionBar(toolbar);
+
+        toolbar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 
     @Override
